c5557f8752f9bf3be06e2aedcb5d971c7580b6ee
Lauren Hetherington <lauren@goodcall.biz>
2023-03-23 14:41:28 +0000

    code to check if a letter is in a randomly selected word

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,12 @@
+from random_word import RandomWords
+
+r = RandomWords()
+word = r.get_random_word()
+print(word)
+
 while True:
     guess = input("enter a single letter: ")
-    if len(guess) == 1 and guess.isalpha():
-         break
+    if len(guess) == 1 and guess.isalpha() and guess in word:
+         print(f"Good Guess! {guess} is in the word.")
     else:
-         print("Invalid lesster, please enter a single alphabetical character")
+         print(f"Sorry, {guess} is not in the word. Try again")
