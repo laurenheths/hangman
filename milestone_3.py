c8bd2ed3f4b56a7abaedb6ee3118dadfb8dd1d85
Lauren Hetherington <lauren@goodcall.biz>
2023-03-25 12:26:45 +0000

    Create 2 functions to ensure letter was in the random word and a valid input

--- milestone_3.py
+++ milestone_3.py
@@ -1,12 +1,23 @@
-from random_word import RandomWords
+import random
 
-r = RandomWords()
-word = r.get_random_word()
-print(word)
+word_list = ["apple", "banana", "pear"]
+word = random.choice(word_list)
 
-while True:
-    guess = input("enter a single letter: ")
-    if len(guess) == 1 and guess.isalpha() and guess in word:
-         print(f"Good Guess! {guess} is in the word.")
-    else:
-         print(f"Sorry, {guess} is not in the word. Try again")
+def check_guess(guess):
+     guess.lower()
+     if guess in word:
+          print(f"Good Guess! {guess} is in the word.")
+     else:
+          print(f"Sorry, {guess} is not in the word. Try again")
+
+def ask_for_input():
+     while True:
+          guess = input("enter a single letter: ")
+          if len(guess) == 1 and guess.isalpha():
+               break
+          else:
+               print("Invalid letter, please enter a single alphabetical letter")
+     check_guess(guess)
+
+ask_for_input()
+
